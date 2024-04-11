d8b8c5e7c6c8e47ff8e09b6d9eca7fee1ab83d2f
Akki-net <akashsharma.zak@gmail.com>
2024-04-11 18:46:18 +0530

    Views

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 
 DATABASE = "blog.db"
@@ -8,6 +8,14 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config["DATABASE"])
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
